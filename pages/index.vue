06b12ac1802c2fb7e1b66936c9061149bbe400dd
santiago <santiagotaschetta@gmail.com>
2021-12-03 11:15:53 -0300

    removed script on /index

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,3 @@
   <main>
   </main>
 </template>
-
-<script setup>
-import { ref, onMounted, useContext } from '@nuxtjs/composition-api'
-
-const { $axios } = useContext()
-
-const items = ref([])
-
-const loadItems = async () => {
-  const result = await $axios.$get('/admins')
-  items.value = result.items
-}
-
-onMounted(() => {
-  loadItems()
-})
-
-</script>
