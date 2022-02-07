54f7221c4f6db469ca37271225efa9b34dc3319b
santiago <santiagotaschetta@gmail.com>
2022-02-07 12:33:09 -0300

    correccion de error en la build

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,5 +65,16 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    // transpile: ['vee-validate'],
+    postcss: { 
+      plugins: { 
+        "postcss-custom-properties": false, 
+        'postcss-preset-env': { 
+          features: { 
+            'custom-properties': false 
+          } 
+        } 
+      } 
+    }
   }
 }
